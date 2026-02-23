bcc5bc3e73fa472ac41295eb786addb37f053269
Amogh Nadakudhiti <amogh.nad@gmail.com>
2026-02-22 18:37:07 -0800

    update

--- stdarray.cpp
+++ stdarray.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <numeric>
+#include <algorithm>
 #include <array>
 using namespace std;
 
@@ -17,18 +19,20 @@ int main(){
     if (fin.good()){
         for (int i = 0; i < numPurchase; i++){
             fin >> purchases[i];
-            cin.ignore();
+            fin.ignore();
             getline(fin, customers[i]);
         }
         fin.close();
     }
-    else
+    else{
         cout << "file not found.";
+        return 1;
+    }
 
-    cout << "Total customer count for the day: " << customers.size();
-
-
-
+    cout << "Total customer count for the day: " << customers.size() << endl;
+    cout << "Biggest sale: " << *max_element(purchases.begin(), purchases.end()) << endl;
+    cout << "Smallest sale: " << *min_element(purchases.begin(), purchases.end()) << endl;
+    cout << "Total Revenue: " << accumulate(purchases.begin(), purchases.end(), 0) << endl;
 
     return 0;
 }
