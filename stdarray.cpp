2ca74e2a196ddbd2e6937fc18c061fc5914b8047
Amogh Nadakudhiti <amogh.nad@gmail.com>
2026-02-22 18:27:02 -0800

    update

--- stdarray.cpp
+++ stdarray.cpp
@@ -5,13 +5,14 @@ using namespace std;
 
 const int numPurchase = 30;
 
+
 int main(){
 
     ifstream fin;
     array <string, numPurchase> customers;
     array <int, numPurchase> purchases;
 
-    fin.open("custoers.txt");
+    fin.open("customers.txt");
     //filling in array with all text file data
     if (fin.good()){
         for (int i = 0; i < numPurchase; i++){
@@ -23,5 +24,11 @@ int main(){
     }
     else
         cout << "file not found.";
+
+    cout << "Total customer count for the day: " << customers.size();
+
+
+
+
     return 0;
 }
