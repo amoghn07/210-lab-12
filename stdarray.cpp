487f408169b4ec76bfd90d280b4aa4d968010d80
Amogh Nadakudhiti <amogh.nad@gmail.com>
2026-02-22 18:47:17 -0800

    update+break

--- stdarray.cpp
+++ stdarray.cpp
@@ -12,7 +12,7 @@ int main(){
 
     ifstream fin;
     array <string, numPurchase> customers;
-    array <int, numPurchase> purchases;
+    array <double, numPurchase> purchases;
 
     fin.open("customers.txt");
     //filling in array with all text file data
@@ -29,10 +29,18 @@ int main(){
         return 1;
     }
 
+    double bigBuy = *max_element(purchases.begin(), purchases.end());
     cout << "Total customer count for the day: " << customers.size() << endl;
-    cout << "Biggest sale: " << *max_element(purchases.begin(), purchases.end()) << endl;
+    cout << "Biggest sale: " << bigBuy << endl;
     cout << "Smallest sale: " << *min_element(purchases.begin(), purchases.end()) << endl;
     cout << "Total Revenue: " << accumulate(purchases.begin(), purchases.end(), 0) << endl;
 
+    array<double, numPurchase>::iterator it;
+    it = find(purchases.begin(), purchases.end(), bigBuy);
+    string bigBuyPerson = customers.at(it - purchases.begin());
+
+    cout << "Person who spent the most money: " << bigBuyPerson << endl;
+    cout << "List of customers: \n";
+    for (string person : customers) cout << '\t' << person << endl;
     return 0;
 }
