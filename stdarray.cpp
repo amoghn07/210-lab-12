3e2946fec3e90f761e8b47ecf85079fbab3afae5
Amogh Nadakudhiti <amogh.nad@gmail.com>
2026-02-22 18:15:16 -0800

    update

--- stdarray.cpp
+++ stdarray.cpp
@@ -11,13 +11,14 @@ int main(){
     array <string, numPurchase> customers;
     array <int, numPurchase> purchases;
 
-    fin.open("customers.txt");
+    fin.open("custoers.txt");
+    //filling in array with all text file data
     if (fin.good()){
-        
-
-
-
-
+        for (int i = 0; i < numPurchase; i++){
+            fin >> purchases[i];
+            cin.ignore();
+            getline(fin, customers[i]);
+        }
         fin.close();
     }
     else
